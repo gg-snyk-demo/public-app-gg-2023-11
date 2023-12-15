4a5afe822471546433a56839e1a60309218ce05e
Carol Danvers <carol-danvers-snyk@protonmail.com>
2023-12-15 12:03:36 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import antigravity
 
-client_id=3MVG908STaSSuFnLoH5wJ593La5fXQu4ycDL.uR35gjVGoXNdE0ssLLxWNYJXKvkhUzGybS8_g1odsO7vVsnL
-client_secret=5235812946354841038
+SEED=9df7f7fa-affd-4910-a3cc-46612b8ead94
 
 def main():
     antigravity.fly()
