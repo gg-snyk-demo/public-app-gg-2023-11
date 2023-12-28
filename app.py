0b75fe85a4909f52c69e41fc308596ca2e2d0fcd
Tony Stark <tony-stark-snyk@protonmail.com>
2023-12-28 04:03:36 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import antigravity
 
-SEED=f6c839cc-28f4-4de9-b8fe-8759d11dba43
+CERT="""-----BEGIN DSA PRIVATE KEY-----
+MIIBugIBAAKBgQDOfMVgPu83tiCAXrdsnI8GaDevSAYMDTFMZ9A4+68t+4KZztaj
+tHuMH3dQXz66mfwt6qYWXrQ0Or9yQX+crTpsu7VuVMYFN9whwAQOMzI/9On5nHKs
+5rEkf88z7SYpNdIh3M/EonYrndYG0mwQ0Bcv7IBiQO1UyswDjuINm6g4WQIVAKpZ
+H9frohQimNOkEIQ/sOT8fgslAoGAMmHwNBFDBKER4joHz0GsmbC0fEWFuqYulYbq
+D7BemQKcdoqKt4tq3gq/3kuA9ujaXCzhCTTePzMAQ2MnwTs1oq7DFt13H4/sizOG
+gnUysBX6PyGb1j6j3xkCYd5JkLileA3VEASiGKAgze6r7+uphO53f3v6nRDxpZjn
+5/6ZMloCgYAt+W4eK3nCoYkjah/MqzBkLF69KnpdS0Ghl5GKo7NluQVm51l2ajRM
+jpJ/YupoyA1bFr7Pi9YLIrQi1jjFM61KuW2yxpPtxfnH8ij45f9c+i22bZAZsVuD
+aQahLu8G/PLjt8Di9x7iA6cmRVK1ZENoYNaRre78iNP3T70H1Q3tPAIUTLjuuiXm
+PnjfzMIDd+32KBPS6sU=
+-----END DSA PRIVATE KEY-----
+"""
 
 def main():
     antigravity.fly()
